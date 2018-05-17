562b75d8824f81a546221a914a6ba1b4dc48f8e8
MAC <phuong.doan@titancorpvn.com>
2018-05-17 12:48:32 +0700

    use pop()

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -4,8 +4,9 @@ import { View, Text, Button } from 'react-native'
 export default class ProfileScreen extends Component {
     render() {
         return (
-            <View style={{ justifyContent: 'center', alignContent: 'center', flex: 1 }}>
-                <Text style={{ alignSelf: 'center' }}>This is Profile screen</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                <Text>This is Profile screen</Text>
+                <Button onPress={() => this.props.navigation.pop()} title='Go Back (pop)'/>
             </View>
         )
     }
